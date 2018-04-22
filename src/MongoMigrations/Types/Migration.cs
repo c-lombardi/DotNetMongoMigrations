de785731dc209aa237c9ad59b40242cbdbd428ce
c-lombardi <clombardi@problemsolutions.net>
2018-04-22 13:14:01 -0400

    added Migration abstraction

---
+++ src/MongoMigrations/Types/Migration.cs
@@ -0,0 +1,16 @@
+﻿using Migrations.Types;
+using MongoDB.Driver;
+
+namespace MongoMigrations.Types
+{
+    public abstract class Migration : Migrations.Types.Migration
+    {
+        protected readonly IMongoDatabase MongoDatabase;
+        public Migration(
+            IMongoDatabase mongoDatabase, 
+            MigrationVersion version, 
+            string description) : base(version, description)
+        {
+        }
+    }
+}
